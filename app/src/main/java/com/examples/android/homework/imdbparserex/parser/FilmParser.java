11bb1c078a0141c8259fcc79c36cac72e41ff4eb
galochkin <A.Galochkin@mit.ooo>
2017-05-19 17:59:24 +0300

    closing stream

--- app/src/main/java/com/examples/android/homework/imdbparserex/parser/FilmParser.java
+++ app/src/main/java/com/examples/android/homework/imdbparserex/parser/FilmParser.java
@@ -2,6 +2,8 @@ package com.examples.android.homework.imdbparserex.parser;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 
 import com.examples.android.homework.imdbparserex.entity.Film;
@@ -13,6 +15,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 /**
@@ -31,19 +34,33 @@ public class FilmParser {
             Document document = Jsoup.connect("http://www.imdb.com/chart/top").get();
             Element element = document.getElementsByAttributeValue("data-caller-name", "chart-top250movie").get(0);
             Elements rowElem = element.getElementsByTag("tr");
+            InputStream is = null;
             int i = 0;
             for (Element elem : rowElem) {
 
                 if (elem == rowElem.get(0)) {
                     continue;
                 }
-               if(rowElem.indexOf(elem)>20)return;
+
+                // load 250 taking too much time
+               if(rowElem.indexOf(elem)>=20)return;
                 title = elem.select(".titleColumn > a").text();
-                String iconURL = elem.select("img").first().absUrl("src");
-                icon = BitmapFactory.decodeStream(new URL(iconURL).openConnection().getInputStream());
                 date = elem.select("span.secondaryInfo").text();
                 rating = elem.select("td.ratingColumn > strong").text();
-                Log.e(TAG, title + " " + icon + " " + date + " " + rating);
+                String iconURL = elem.select("img").first().absUrl("src");
+
+                try {
+                    is = new URL(iconURL).openConnection().getInputStream();
+                    icon = BitmapFactory.decodeStream(is);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (is != null){
+                        is.close();
+                    }
+                }
+
+                Log.d(TAG, title + " " + icon + " " + date + " " + rating);
                 result.addFilm(new Film(icon,title,date,rating));
             }
 
