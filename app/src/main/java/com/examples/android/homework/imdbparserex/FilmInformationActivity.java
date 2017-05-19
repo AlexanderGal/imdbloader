1e289928f3265ebb2177bef676cd22b1ff28fcd1
Ural Nigmatullin <e_nigm@list.ru>
2017-05-19 22:24:32 +0300

    filminformation added

---
+++ app/src/main/java/com/examples/android/homework/imdbparserex/FilmInformationActivity.java
@@ -0,0 +1,94 @@
+package com.examples.android.homework.imdbparserex;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.examples.android.homework.imdbparserex.entity.FilmInformation;
+import com.examples.android.homework.imdbparserex.loader.FilmInformationLoader;
+import com.examples.android.homework.imdbparserex.parser.FilmInformationParser;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by enigm777 on 19.05.2017.
+ */
+
+public class FilmInformationActivity extends AppCompatActivity {
+
+    private static final String TAG = "FilmInformationActivity";
+    private static final int FILM_INFORMATION_LOADER_ID = 777;
+
+    private TextView mMovieTitle;
+    private ImageView mMovieIcon;
+    private TextView mMovieYear;
+    private TextView mMovieRating;
+    private TextView mMovieVotes;
+    private TextView mMovieActors;
+    private TextView mMoviePlot;
+
+    private ProgressBar mProgressBar;
+
+    private URL mFilmInformationUrl;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.film_information);
+
+        mMovieTitle = (TextView) findViewById(R.id.movie_title);
+        mMovieIcon = (ImageView) findViewById(R.id.movie_icon);
+        mMovieYear = (TextView)findViewById(R.id.movie_year);
+        mMovieRating = (TextView)findViewById(R.id.imdb_rating);
+        mMovieVotes = (TextView)findViewById(R.id.imdb_votes);
+        mMovieActors = (TextView)findViewById(R.id.movie_actors);
+        mMoviePlot = (TextView)findViewById(R.id.movie_plot);
+
+        mProgressBar = (ProgressBar)findViewById(R.id.movie_info_loading);
+
+        try {
+            mFilmInformationUrl = new URL("http://www.omdbapi.com/?t=The+Shawshank+Redemption&plot=full");
+        } catch(IOException ex){
+            Log.e(TAG,ex.getMessage());
+        }
+
+        getSupportLoaderManager().initLoader(FILM_INFORMATION_LOADER_ID,null,new FilmInformationLoaderCallbacks()).forceLoad();
+
+
+    }
+
+    private class FilmInformationLoaderCallbacks implements LoaderManager.LoaderCallbacks<FilmInformation>{
+
+        @Override
+        public Loader<FilmInformation> onCreateLoader(int id, Bundle args) {
+            return new FilmInformationLoader(FilmInformationActivity.this,mFilmInformationUrl,new FilmInformationParser());
+        }
+
+        @Override
+        public void onLoadFinished(Loader<FilmInformation> loader, FilmInformation data) {
+            mProgressBar.setVisibility(View.GONE);
+            mMovieTitle.setText(data.getMovieTitle());
+            mMoviePlot.setText(data.getMoviePlot());
+            mMovieActors.setText(data.getMovieActors());
+            mMovieRating.setText(data.getMovieRating());
+            mMovieVotes.setText(data.getMovieVotes());
+            mMovieYear.setText(data.getMovieYear());
+            mMovieIcon.setImageBitmap(data.getMovieIcon());
+
+            Log.d(TAG,"onLoadFinished");
+        }
+
+        @Override
+        public void onLoaderReset(Loader<FilmInformation> loader) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/examples/android/homework/imdbparserex/loader/FilmInformationLoader.java
@@ -0,0 +1,43 @@
+package com.examples.android.homework.imdbparserex.loader;
+
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
+
+import com.examples.android.homework.imdbparserex.entity.FilmInformation;
+import com.examples.android.homework.imdbparserex.parser.FilmInformationParser;
+
+import java.lang.ref.WeakReference;
+import java.net.URL;
+
+/**
+ * Created by enigm777 on 19.05.2017.
+ */
+
+public class FilmInformationLoader extends AsyncTaskLoader<FilmInformation> {
+    private static final String TAG = "FilmInformationLoader";
+
+    private URL filmInfoUrl;
+    WeakReference<FilmInformationParser> mParserWeakReference;
+
+
+    public FilmInformationLoader(Context context, URL filmInfoUrl, FilmInformationParser filmInformationParser) {
+        super(context);
+        this.mParserWeakReference = new WeakReference<FilmInformationParser>(filmInformationParser);
+        this.filmInfoUrl = filmInfoUrl;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @Override
+    public FilmInformation loadInBackground() {
+        FilmInformationParser filmInformationParser = mParserWeakReference.get();
+        Log.d(TAG,"loadInBackground ");
+        return filmInformationParser.parseInfo(filmInfoUrl);
+    }
+}
